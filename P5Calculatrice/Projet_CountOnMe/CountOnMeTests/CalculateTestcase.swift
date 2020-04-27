c6c7ce288e1eae0d389a1d25bf28294186bdbfb0
adam <ad.mabrouki@gmail.com>
2020-04-27 17:34:31 +0200

    rajout de la division par zero, et la priorité des operateurs

--- P5Calculatrice/Projet_CountOnMe/CountOnMeTests/CalculateTestcase.swift
+++ P5Calculatrice/Projet_CountOnMe/CountOnMeTests/CalculateTestcase.swift
@@ -15,31 +15,32 @@ class CalculateTestcase: XCTestCase {
         calcultest = Calculate()
     }
     
-    
+    /// add number test
     func testGivenAddNumberOneToDisplay_WhenButtonOneTapped_textViewEqualNumberOneTapped() {
         calcultest.addNumber(numberText: "1")
         XCTAssert(calcultest.text == "1")
     }
     
-    
+    /// add operateur plus test
     func testGivenCalculIsOnePlusOne_WhenCalculate_resultIsTwo() {
         calcultest.addNumber(numberText: "1")
         calcultest.addOperator(operatoree: .addition)
         calcultest.addNumber(numberText: "1")
         calcultest.total()
-        XCTAssert(calcultest.text == "1 + 1 = 2.0")
+        XCTAssert(calcultest.text == "1 + 1 = 2")
         
     }
     
+    /// add operator minus test
     func testGivenCalculIsTwoMinusOne_WhenCalculate_resultIsOne() {
         calcultest.addNumber(numberText: "2")
         calcultest.addOperator(operatoree: .subtraction)
         calcultest.addNumber(numberText: "1")
         calcultest.total()
-        XCTAssert(calcultest.text == "2 - 1 = 1.0")
+        XCTAssert(calcultest.text == "2 - 1 = 1")
     }
     
-    
+    /// add operator multiply test
         func testGivenCalculIsTwoMulipllyByTWO_WhenCalculate_ResultEqualFour() {
              calcultest.addNumber(numberText: "2")
             calcultest.addOperator(operatoree: .multiplication)
@@ -49,6 +50,7 @@ class CalculateTestcase: XCTestCase {
     
         }
     
+    /// add operator divide test
         func testGivenCalculIsFourDividebyTwo_WhenCalculate_ResultIsTwo() {
              calcultest.addNumber(numberText: "4")
             calcultest.addOperator(operatoree: .division)
@@ -57,6 +59,7 @@ class CalculateTestcase: XCTestCase {
             XCTAssert(calcultest.text == "4 ÷ 2 = 2.0")
         }
     
+    /// reset button test
     func testGivenWhenACButtonPressed_textViewIsEmpty() {
         calcultest.addNumber(numberText: "4")
         calcultest.addOperator(operatoree: .division)
@@ -65,8 +68,7 @@ class CalculateTestcase: XCTestCase {
         
     }
     
-    
-    
+    /// divide buy 0 test
     func testGivenWhenNumberIsDivideByZero_WhenCalculate_TexviewIsEmpty() {
         
         calcultest.addNumber(numberText: "8")
@@ -75,13 +77,41 @@ class CalculateTestcase: XCTestCase {
         calcultest.total()
         XCTAssert(calcultest.text == "")
     }
-    //    
-    //    func testGivenWhenResultIsDisplay_PressNewButtonNumber_TextViewDisplayNewButtonNumber() {
-    //        
-    //    }
     
-    //    tester priorité des operateurs en les utilisants tous
-    //    tester le bouton A/C
-    //    tester si le texte ne donne pas de calcul si aucun opétateur entré apres le premier nombre
-    //
+  
+    func testGivenCalculTextAddOperator_WhenTwoOperatorAreTapped_ThendNotificationAppear() {
+        calcultest.addNumber(numberText: "2")
+        calcultest.addOperator(operatoree: .subtraction)
+        calcultest.addOperator(operatoree: .subtraction)
+        XCTAssert(calcultest.text == "2 - ")
+     }
+    
+    func testGivenExpressionHaveEnoughElement_WhenMissElements_ThendNotificationAppear() {
+        calcultest.addNumber(numberText: "5")
+        calcultest.total()
+         XCTAssert(calcultest.text == "5")
+       
+    }
+  
+    func testGivenAddOperator_WhenOperationHaveResult_ThenShouldNotification() {
+        calcultest.addNumber(numberText: "1")
+              calcultest.addOperator(operatoree: .addition)
+              calcultest.addNumber(numberText: "1")
+              calcultest.total()
+         calcultest.addOperator(operatoree: .addition)
+              XCTAssert(calcultest.text == "1 + 1 = 2 + ")
+        
+
+    }
+    
+    func testGivenCalculisOneUndreedDivideBy3_WhenCalculate_ResultIsMultipleElementAfterVirgule() {
+        calcultest.addNumber(numberText: "100")
+        calcultest.addOperator(operatoree: .division)
+        calcultest.addNumber(numberText: "3")
+        calcultest.total()
+        XCTAssert(calcultest.text == "100 ÷ 3 = 33.333333333333336")
+    }
+    
+    
+    // ajouter le test des grands nombre 
 }
--- P5Calculatrice/Projet_CountOnMe/model/Calculate.swift
+++ P5Calculatrice/Projet_CountOnMe/model/Calculate.swift
@@ -32,7 +32,6 @@ class Calculate {
     
     var elements: [String] {
         return text.split(separator: " ").map { "\($0)" }
-        
     }
     
     
@@ -46,11 +45,15 @@ class Calculate {
     }
     
     var canAddOperator: Bool {
-        return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "÷"
+    return elements.last != "+" && elements.last != "-" && elements.last != "*" && elements.last != "÷"
     }
     
     var expressionHaveResult: Bool {
         return text.firstIndex(of: "=") != nil
+    }
+    
+    var divideByZero: Bool {
+        return text.contains("÷ 0")
     }
     
     
@@ -61,39 +64,53 @@ class Calculate {
             text = ""
         }
         text.append(numberText)
-        
-        
     }
     
     func addOperator(operatoree:operators)  {
-        
+        if expressionHaveResult || text.isEmpty {
+            delegate?.alertMessage(text: "Vous ne pouvez pas ajouter d'operateur ")
+           
+        }
         if canAddOperator {
             text.append(" " + operatoree.rawValue + " ")
             
             
         } else {
             delegate?.alertMessage(text: "un opérateur est déja mis ")
-         
-        }
-       
-    }
-    
-   var divideByZero: Bool {
-        return text.contains("÷ 0")
-    }
             
-    
-        
-        
-        
+        }
+    }
     
     func refresh ()   {
         text = ""
     }
     
-    func total()  {
+    func priority(expression: [String]) -> [String] {
+        var priorExpression: [String] = expression
         
-
+        while priorExpression.contains("*") || priorExpression.contains("÷") {
+            if let index = priorExpression.firstIndex(where: {$0 == "*" || $0 == "÷" } ) {
+                let operand = priorExpression[index]
+                guard let leftSide = Double(priorExpression[index - 1]) else { return [] }
+                guard let rightSide = Double(priorExpression[index + 1]) else { return [] }
+                
+                let calcul: Double
+                if operand == "*" {
+                    calcul = leftSide * rightSide
+                } else {
+                    calcul = leftSide / rightSide
+                }
+                priorExpression[index - 1] = String(calcul)
+                priorExpression.remove(at: index + 1)
+                priorExpression.remove(at: index)
+                
+            }
+            
+        }
+        return priorExpression
+    }
+    
+    func total()  {
         guard expressionIsCorrect else {
             delegate?.alertMessage(text: "Entrez une expréssion correcte!")
             return
@@ -103,41 +120,44 @@ class Calculate {
             
             return
         }
-        guard !divideByZero else { // why exclamation point 
-                   delegate?.alertMessage(text: "La division par zéro n'existe pas")
-             text = ""
+        guard !divideByZero else {
+            delegate?.alertMessage(text: "La division par zéro n'existe pas")
+            text = ""
             return
         }
-        
         // Create local copy of operations
-        var operationsToReduce = elements
-//
-        // Iterate over operations while an operand still here
+        var operationsToReduce = priority(expression: elements)
         while operationsToReduce.count > 1 {
             let left = Double(operationsToReduce[0])! // debalé correctement
             let operand = operationsToReduce[1]
             let right = Double(operationsToReduce[2])! // debalé correctement
-            
             let result: Double
             switch operand {
             case "+": result = left + right
             case "-": result = left - right
-            case "*": result = left * right
-            case "÷": result = left / right
             default: return
             }
             
             operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
+            operationsToReduce.insert(formatResult(result: Double(result)), at: 0)
+         
         }
         
         text.append(" = \(operationsToReduce.first!)")
-        
-        //            return self.present(alertVC, animated: true, completion: nil); text
-        
     }
     
-    
+     //// alow long numbers display
+    private func formatResult(result: Double) -> String {
+        let formatter = NumberFormatter()
+        formatter.maximumFractionDigits = 3
+        
+        guard let resultFormated = formatter.string(from: NSNumber(value: result)) else { return String() }
+        
+        guard resultFormated.count <= 10 else {
+            return String(result)
+        }
+        return resultFormated
+    }
 }
 
 
